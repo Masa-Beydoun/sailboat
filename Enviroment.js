1c080c004c688c7d0751a69ede793ccafb3eb2f8
Masa <masa.beydoun@hotmail.com>
2024-07-15 18:42:07 +0300

    water forces

---
+++ Enviroment.js
@@ -0,0 +1,90 @@
+import * as dat from "dat.gui";
+
+class Enviroment {
+    constructor() {
+
+        //general variables
+        this.passengerMass = 0;
+        this.equipmentMass = 500;
+        this.gravityConstant = 9.81;
+        this.waterDensity = 1000;
+
+
+
+
+        //boat variables
+        this.length = 8;
+        this.width = 4;
+        this.surfaceAreaSpace = this.l * this.w;
+        this.velocity = 1000;
+
+        //water speed
+        this.waterSpeedX = 1;
+        this.waterSpeed_X = 1;
+        this.waterSpeedZ = 1;
+        this.waterSpeed_Z = 1;
+
+
+        //water forces variables
+        this.besideVolume = 0.53;
+        this.cd = 0.1;
+
+        // متغيرات قوة برنولي
+        this.p0 = 13;
+        this.v0 = 13;
+
+
+
+        this.gui = new dat.GUI();
+        this.gui
+            .add(this, "waterDensity")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("waterDensity ");
+        this.gui
+            .add(this, "length")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("length ");
+        this.gui
+            .add(this, "width")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("width");
+        this.gui
+            .add(this, "gravityConstant")
+            .min(-10)
+            .max(10)
+            .step(1)
+            .name("gravityConstant ");
+        this.gui
+            .add(this, "waterSpeedX")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("waterSpeedX ");
+        this.gui
+            .add(this, "waterSpeedZ")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("waterSpeedZ ");
+        this.gui
+            .add(this, "waterSpeed_X")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("waterSpeed-X ");
+        this.gui
+            .add(this, "waterSpeed_Z")
+            .min(0)
+            .max(1000)
+            .step(1)
+            .name("waterSpeed-Z ");
+    }
+}
+
+export default Enviroment;
---
+++ WaterForces.js
@@ -0,0 +1,142 @@
+import { Vector3 } from "three";
+
+import * as dat from "dat.gui";
+
+import enviroment from "./Enviroment";
+class WaterForce {
+
+    constructor() {
+
+        this.startSimulation = false;
+        this.position = new Vector3(0, 0, 0);
+        this.velocity = new Vector3(1, 1, 1);
+        this.accelration = new Vector3(0, 0, 0);
+
+        this.gui = new dat.GUI();
+
+        this.gui
+            .add(this, "startSimulation")
+            .name("start simulation ");
+    }
+
+
+
+
+    calculateBuoyantForce() {
+        //قوة الطفو = كثافة المي * ثابت الجاذبية الأرضية * حجم السائل المزاح
+        // TODO : edit حجم السائل المزاح
+        let buoyant_Force = enviroment.waterDensity * enviroment.gravityConstant * enviroment.besideVolume;
+
+        let buoyantForceVector = new Vector3(0, buoyant_Force, 0);
+
+        return buoyantForceVector;
+    }
+
+
+    calculateWeightOfBoat() {
+        // كتلة الحمولة = كتلة الركاب + كتلة المعدات
+        var boatMass = enviroment.passengerMass + enviroment.equipmentMass;
+
+        // قوة الثقل = كتلة المنطاد × ثابت الجاذبية الأرضية
+        var weightOfBoat = boatMass * enviroment.gravityConstant;
+        var weightVector = new Vector3(0, -weightOfBoat, 0);
+
+        return weightVector;
+    }
+
+
+
+
+
+
+    calculateWaterForceX() {
+        let relativeVelocity = this.velocity.clone();
+        relativeVelocity.x += enviroment.waterSpeedX;
+
+        let waterForce = 0.5 * enviroment.cd * enviroment.waterDensity * enviroment.surfaceAreaSpace * Math.pow(enviroment.waterSpeedX, 2);
+        // قوة الماء =  معامل السحب الديناميكي × كثافة الماء × مساحة السطح المتأثر × سرعة الماء للتربيع 
+
+        let waterForceVector = new Vector3(waterForce, 0, 0);
+
+        return waterForceVector;
+    }
+
+    calculateWaterForce_X() {
+        let relativeVelocity = this.velocity.clone();
+        relativeVelocity.x += enviroment.waterSpeed_X;
+
+        let waterForce = 0.5 * enviroment.cd * enviroment.waterDensity * enviroment.surfaceAreaSpace * Math.pow(relativeVelocity.x, 2);
+
+        let waterForceVector = new Vector3(-waterForce, 0, 0);
+
+        return waterForceVector;
+    }
+
+    calculateWaterForceZ() {
+        let relativeVelocity = this.velocity.clone();
+        relativeVelocity.z += enviroment.waterSpeedZ;
+
+        let waterForce = 0.5 * enviroment.cd * enviroment.waterDensity * enviroment.surfaceArea * Math.pow(relativeVelocity.z, 2);
+
+        let waterForceVector = new Vector3(0, 0, waterForce);
+
+        return waterForceVector;
+    }
+    calculateWaterForce_Z() {
+        let relativeVelocity = this.velocity.clone();
+        relativeVelocity.z += this.waterSpeed_Z;
+
+        let waterForce = 0.5 * enviroment.cd * enviroment.waterDensity * enviroment.surfaceArea * Math.pow(relativeVelocity.z, 2);
+
+        let waterForceVector = new Vector3(0, 0, -waterForce);
+
+        return waterForceVector;
+    }
+
+
+
+    calculateWaterResistance() {
+        let waterResistance = 0.5 * enviroment.waterDensity * enviroment.surfaceArea * Math.pow(enviroment.velocity.lengthSq(), 2);
+        let waterResistanceVector = new Vector3(0, waterResistance, 0);
+
+        return waterResistanceVector;
+    }
+
+
+    calculateBernoly() {
+        let p = enviroment.p0 - 0.5 * enviroment.waterDensity * Math.pow((velocity.lengthSq() - enviroment.v0), 2);
+    }
+
+
+
+
+
+    totalForce() {
+        var tf = new Vector3(0, 0, 0);
+        tf = tf.add(this.calculateWeightOfBoat());
+        tf = tf.add(this.calculateWaterResistance());
+        tf = tf.add(this.calculateWaterForceX());
+        tf = tf.add(this.calculateWaterForceZ());
+        tf = tf.add(this.calculateWaterForce_X());
+        tf = tf.add(this.calculateWaterForce_Z());
+
+        return tf;
+    }
+
+    update() {
+        var delta = 0.01666666666666666666666666666667;
+        var tf = this.totalForce();
+        console.log("weightVector", this.calculateWeightOfBoat());
+        console.log("BuoyantForce", this.calculateBuoyantForce());
+        console.log("waterResistanceVector", this.calculateWaterResistance());
+        console.log("WaterForceX", this.calculateWaterForceX());
+        console.log("WaterForceZ", this.calculateWaterForceZ());
+
+    }
+
+
+
+}
+
+
+export default WaterForce;
