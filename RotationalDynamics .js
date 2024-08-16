62cc4a3bfe3065b8a477eb06cc0bb0093ed53167
Masa <masa.beydoun@hotmail.com>
2024-08-16 13:32:49 +0300

    rational starts working

--- RotationalDynamics .js
+++ RotationalDynamics .js
@@ -17,7 +17,6 @@ class RotationalDynamics {
         this.enviroment.torque.z = waterForce.z * pointOfApplicationWater.z * this.enviroment.dfa;
         this.enviroment.torque.x = windForce.x * pointOfApplicationWind.x - waterForce.x * pointOfApplicationWater.x;
 
-        console.log("torque ", this.enviroment.torque);
     }
 
 
@@ -29,31 +28,20 @@ class RotationalDynamics {
             this.enviroment.torque.y / this.enviroment.momentOfInertia.y,
             this.enviroment.torque.z / this.enviroment.momentOfInertia.z
         );
-        console.log("angular Acceleration", this.enviroment.angularAcceleration);
     }
     calculateVelocity() {
         this.enviroment.angularVelocity.addScaledVector(this.enviroment.angularAcceleration, this.enviroment.deltaTime);
-        console.log("angular velocity", this.enviroment.velocity);
     }
     calculateTheta() {
 
         this.enviroment.alpha = (this.enviroment.angularVelocity.z * this.enviroment.deltaTime) + (0.5 * Math.pow(this.enviroment.deltaTime, 2) * this.enviroment.angularAcceleration.z);
         this.enviroment.beta = (this.enviroment.angularVelocity.x * this.enviroment.deltaTime) + (0.5 * Math.pow(this.enviroment.deltaTime, 2) * this.enviroment.angularAcceleration.x);
-        console.log("alpha", this.enviroment.alpha);
-        console.log("beta", this.enviroment.beta);
     }
 
 
     updateRotation() {
-
-        // Create quaternions for each rotation
-        // let qAlpha = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), this.enviroment.alpha); // Rotate around X-axis
-        // let qBeta = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), this.enviroment.beta);  // Rotate around Y-axis
-        this.enviroment.boatRotation.y = this.enviroment.alpha; // Rotation around Y-axis
-        this.enviroment.boatRotation.z = this.enviroment.beta;  // Rotation around Z-axis
-
-
-
+        this.enviroment.boatRotation.y = this.enviroment.alpha;
+        this.enviroment.boatRotation.z = this.enviroment.beta;
     }
 
     update(waterForce, windForce) {
@@ -64,50 +52,5 @@ class RotationalDynamics {
         this.updateRotation();
     }
 }
-
-/* final code form gpt
-
-
-// تعريف قوة الرياح وموقع تطبيقها
-const windForce = new THREE.Vector3(0, 1000, 0); // قوة الرياح في الاتجاه Y (نيوتن)
-const pointOfApplication = new THREE.Vector3(2.5, 0, 0); // موقع القوة بالنسبة لمركز الكتلة (متر)
-
-// لحظة القصور الذاتي (افترض لحظة القصور الذاتي للقارب على محور Z)
-const momentOfInertia = (1 / 12) * (5 * Math.pow(2, 2) + 2 * Math.pow(1, 2)); // لحظة القصور الذاتي لمتوازي المستطيلات
-
-// حساب العزم الزاوي حول المحور الرأسي (محور Z)
-const torque = new THREE.Vector3().crossVectors(pointOfApplication, windForce);
-const torqueMagnitude = torque.length();
-
-// حساب التسارع الزاوي
-const angularAcceleration = torqueMagnitude / momentOfInertia;
-
-// متغيرات للسرعة الزاوية والزاوية الدوارة
-let angularVelocity = 0; // السرعة الزاوية الابتدائية
-let initialTime = Date.now(); // الوقت الابتدائي
-
-// وظيفة لتحديث الرسم البياني
-function animate() {
-    requestAnimationFrame(animate);
-
-    // حساب الوقت المنقضي
-    const elapsedTime = (Date.now() - initialTime) / 1000; // الوقت بالثواني
-
-    // حساب السرعة الزاوية
-    angularVelocity = angularVelocity + angularAcceleration * elapsedTime;
-
-    // حساب الزاوية الدوارة
-    const angle = angularVelocity * elapsedTime + 0.5 * angularAcceleration * Math.pow(elapsedTime, 2);
-
-    // تحديث دوران القارب
-    boat.rotation.z = angle;
-
-    renderer.render(scene, camera);
-}
-animate();
-
-
-
-*/
 
 export default RotationalDynamics;
